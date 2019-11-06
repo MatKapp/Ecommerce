1f45aa5b94952e8b219063d12e47aa0eb86bcbb9
MatKapp <mka@bcsg.pl>
2019-11-06 11:20:50 +0100

    Added Firebase modules

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD9sfMg-Ju1RczepVTQqDh2_qiinxe6AZg",
+    authDomain: "ecommerce-69cac.firebaseapp.com",
+    databaseURL: "https://ecommerce-69cac.firebaseio.com",
+    projectId: "ecommerce-69cac",
+    storageBucket: "ecommerce-69cac.appspot.com",
+    messagingSenderId: "367946304880",
+    appId: "1:367946304880:web:998575caa3059060fdd3b5"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD9sfMg-Ju1RczepVTQqDh2_qiinxe6AZg",
+    authDomain: "ecommerce-69cac.firebaseapp.com",
+    databaseURL: "https://ecommerce-69cac.firebaseio.com",
+    projectId: "ecommerce-69cac",
+    storageBucket: "ecommerce-69cac.appspot.com",
+    messagingSenderId: "367946304880",
+    appId: "1:367946304880:web:998575caa3059060fdd3b5"
+  }
 };
 
 /*
